8be0617f1a0bcc4ff17075d96dbac70b33b66982
Yurly123 <joo7won@gmail.com>
2022-12-13 22:02:44 +0900

    learned useEffect of react object

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,11 +1,25 @@
-import Background from './Background';
-import Clock from './clock';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [keyword, setKeyword] = useState('')
+  useEffect(() => {
+    console.log('I run only once')
+  }, [])
+  useEffect(() => {
+    console.log('I run when [keyword] changes')
+  }, [keyword])
+  useEffect(() => {
+    console.log('I run when [counter] changes')
+  }, [counter])
+
+  const onClick = () => setCounter(prev => prev + 1) 
+  const onChange = (eventArg) => setKeyword(eventArg.target.value)
   return (
     <div>
-      <Clock />
-      <Background />
+      <input type="text" placeholder="Search" onChange={onChange} value={keyword}></input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click Me!</button>
     </div>
   );
 }
